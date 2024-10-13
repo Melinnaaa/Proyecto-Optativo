c2801af638a8a00c811647760e574ada4a9aca8c
JavierMarin24 <javier.marin.m@mail.pucv.cl>
2024-10-12 23:56:31 -0300

    Funcionalidad de botones

---
+++ Assets/Scripts/BotonCambioEscena.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BotonCambioEscena : MonoBehaviour
+{
+
+
+
+
+    public void CargarMenu()
+    {
+        
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void Comenzar()
+    {
+        
+        SceneManager.LoadScene("Level1");
+    }
+    public void proxLevel()
+    {
+        
+        SceneManager.LoadScene("Level2");
+    }
+}
